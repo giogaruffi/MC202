de069f99be098fadb12e7ee51b49e545ef29ba90
Flyer <giogaruffi@users.noreply.github.com>
2014-10-15 16:48:40 -0300

    lab04, in progress...

---
+++ quick.c
@@ -0,0 +1,24 @@
+/******************************************
+* Nome: Giovani Garuffi Rondel            *
+* RA: 155559                              *
+* Laboratorio 04 - Quicksort aleatorizado *
+*******************************************/
+
+/* STRUCS, DEFINIÇÕES E CABEÇALHOS DAS SUAS FUNÇÕES DA TAD */
+typedef struct _ex {
+
+} Ex;
+
+void funcao_exemplo(int a);
+// assinatura de outras funções
+
+/* SEU PROGRAMA - MAIN */
+int main() {
+  return 0;
+}
+
+/* IMPLEMENTAÇÃO DAS FUNÇÕES DE SUA TAD */
+void funcao_exemplo(int a) {
+
+}
+// implementação de outras funções
